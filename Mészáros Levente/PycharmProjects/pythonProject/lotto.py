55ad82e7fc4dd2f7d23c783e8dc8549df66a4180
meszaros16 <levjuce2004@gmail.com>
2022-09-15 19:44:11 +0200

    lottózó program

---
+++ Mészáros Levente/PycharmProjects/pythonProject/lotto.py
@@ -0,0 +1,69 @@
+import random
+correct = 0
+
+randomlist = []
+for i in range(2):
+ n = random.randint(1, 90)
+ n2 = random.randint(1, 90)
+ n3 = random.randint(1, 90)
+ n4 = random.randint(1, 90)
+ n5 = random.randint(1, 90)
+randomlist.append(n)
+randomlist.append(n2)
+randomlist.append(n3)
+randomlist.append(n4)
+randomlist.append(n5)
+print(randomlist)
+
+szam = int(input("Adj meg egy számot!"))
+szam2 = int(input("Adj meg egy második számot!"))
+szam3 = int(input("Adj meg egy harmadik számot!"))
+szam4 = int(input("Adj meg egy negyedik számot!"))
+szam5 = int(input("Adj meg egy ötödik számot!"))
+
+if szam == n:
+    correct += 1
+
+elif szam != n:
+    correct += 0
+
+if szam2 == n2:
+    correct += 1
+
+elif szam2 != n2:
+    correct += 0
+
+if szam3 == n3:
+    correct += 1
+
+elif szam3 != n3:
+    correct += 0
+
+if szam4 == n4:
+    correct += 1
+
+elif szam4 != n4:
+    correct += 0
+
+if szam5 == n5:
+    correct += 1
+
+elif szam5 != n5:
+    correct += 0
+
+if correct == 5:
+    print("gratulálok, te vagy a nyertes")
+
+elif correct < 5:
+    print("sajnálom, vesztettél")
+
+
+
+
+
+
+
+
+
+
+
