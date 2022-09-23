7e118613374100e26932a9eb8437385ae61786ff
Jakab Mate Tibor <tibormate.jakab@katonaj-mkszig.hu>
2022-09-23 09:31:10 +0200

    lista

---
+++ Jakab Máté/listak.py
@@ -0,0 +1,16 @@
+honapok = ['januar', 'februar', 'marcius', 'aprilis',]
+    #print(honapok)
+#print(','.join(honapok))
+#print(len(honapok))
+#print(honapok[:-1])
+#szo = "almafa"
+print(szo[:-3])
+
+gyumolcsok = []
+
+gyumolcs = None
+while gyumolcs !='':
+    if gyumolcs != '':
+        gyumolcsok.append(gyumolcs)
+
+print(gyumolcsok)
