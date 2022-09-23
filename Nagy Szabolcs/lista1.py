19aafa19f3f3a702d8875806b16f640c3d4d0d9e
Nagy Szabolcs <szabolcs.nagy@katonaj-mkszig.hu>
2022-09-23 10:26:10 +0200

    lista feladatok

---
+++ Nagy Szabolcs/lista1.py
@@ -0,0 +1,10 @@
+keresztnevek = []
+
+keresztnev = None
+while keresztnev != '':
+    keresztnev = input('Adj meg egy keresztnevet! ')
+    if keresztnev != '':
+
+        keresztnevek.append(keresztnev)
+
+print(keresztnevek)
