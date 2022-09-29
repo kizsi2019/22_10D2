ce446786f49a79116fda6744e05693630b68c39f
jankovicsdavid2 <davidloci5@gmail.com>
2022-09-29 09:40:20 +0200

    anyátokat

--- Jankovics Dávid/pyton/ciklusok2.py
+++ Jankovics Dávid/pyton/ciklusok2.py
@@ -6,7 +6,7 @@ while sor <= 7:
   while oszlop <= darab_karakter:
       print('O ', end='')
       oszlop = oszlop + 1
-      print('')
-      darab_karakter = darab_karakter + 1
-      sor = sor + 1     
+  print('')
+  darab_karakter = darab_karakter + 1
+  sor = sor + 1     
   
