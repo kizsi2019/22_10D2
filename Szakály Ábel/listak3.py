e579612d527c521c3ce3a1d308864a410249ca98
SzakalyAbel <szakalyabel25@gmail.com>
2022-10-03 13:36:53 +0200

    listak bejarasa

--- Szakály Ábel/listak3.py
+++ Szakály Ábel/listak3.py
@@ -14,12 +14,32 @@
 #for honap in honapok:
     #print(index, honap)
     #index = index +1
-honapok = ['január', 'február', 'március', 'április', 'május']
-for index in range(len(honapok)):
-    honapok[index] = honapok[index].upper()
-print(honapok)
+#honapok = ['január', 'február', 'március', 'április', 'május']
+#for index in range(len(honapok)):
+    #honapok[index] = honapok[index].upper()
+#print(honapok)
 #for honap in honapok:
     #honap = honap.upper()
     #print(honap)
 #print(honapok)
 
+#for i in range(1,41):
+    #if i%3 == 0:
+        #print(i)
+#szavak = ['Tamás', 'Ottó', 'tesla', 'Anna', 'tojás', 'kerékpár',]
+#for szo in szavak:
+   #if szo[0] == "T" or szo[0] == "t":
+        #print(szo)
+
+
+#szavak = ['Tamás', 'Ottó', 'tesla', 'Anna', 'tojás', 'kerékpár',]
+#szo_lista = []
+#for szo in szavak:
+    #if szo[0] == "T" or szo[0] == "t":
+        #szo_lista.append(szo)
+#print(szo_lista)
+
+szamok = [120, 9, 5, 24, 6, 17, -45, 92, 15, 48, 57]
+for szam in szamok:
+    if szam %3 == 0 and szam %2 == 0:
+        print(szam)
