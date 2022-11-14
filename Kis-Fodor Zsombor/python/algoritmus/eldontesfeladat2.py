7a2bb762c2ce67e7f19ab252c3c31642bb3f02f1
PayloadTheDecepticon <zsomborkissfodor@gmail.com>
2022-11-14 13:30:53 +0100

    sexy time

--- Kis-Fodor Zsombor/python/algoritmus/eldontesfeladat2.py
+++ Kis-Fodor Zsombor/python/algoritmus/eldontesfeladat2.py
@@ -1,4 +1,22 @@
-word = "amogus"
+import random
+
+words = ["trams", "amogus", "transformers", "rally", "Firebird"]
+word = words[random.randint(0,4)]
+if word == "trams":
+    print("\u001b[31m funny transport")
+elif word == "amogus":
+    print("\u001b[32m a meme based off a hit 2018 game")
+elif word == "transformers":
+    print("\u001b[33m a more than 40 year old franchise based on robot toys")
+elif word == "rally":
+    print("\u001b[32m a type of a car race, usually happening on dirt")
+else:
+    print("\u001b[33m a word similar to Phoenix, it was also a name for a muscle car")
+
+print("\u001b[37m ------")
+print("guess ONE letter from these words!!")
+print("------")
+
 guessedletter = 'none'
 guessedwell = 0
 wrongguesses = 0
@@ -7,11 +25,11 @@ while guessedwell == 0:
     guessedletter = input("give letter")
     for letter in word:
         if letter == guessedletter:
-            print("noice")
+            print("------")
+            print("you got it right! the word was:", word, "!")
             guessedwell = 1
-        else:
-            if guessedwell == 0:
-                wrongguesses += 1
+    if guessedwell == 0:
+        wrongguesses += 1
 
-print('you got', wrongguesses, "wrong guesses")
-print(word)
+print('it took you', wrongguesses, "tries to get one letter right")
+
