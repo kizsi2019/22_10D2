974bc97821ea2d8b1a5885d6a20adb6d8b6cf396
UivarosiGabriel <gabi455@yahoo.com>
2022-11-21 12:35:31 +0100

    6f

---
+++ Uivárosi Gábriel/Szamlalas.py
@@ -0,0 +1,25 @@
+Lisztt = []
+b = 0
+import random
+a = 0
+for i in range(5):
+    a =random.randint(1,10)
+    Lisztt.append(a)
+for szam in Lisztt:
+    if szam % 2 == 0:
+        b = b + 1
+print(Lisztt, "Páros számok:",b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Liszt = ["Ajtó", "alma" , "Amir", "atila" ,"Amerika"]
