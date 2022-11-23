22879e759fef988c9f4e92efb3c3b56a397956e3
Gabi4555 <90388665+Gabi4555@users.noreply.github.com>
2022-11-23 22:05:54 +0100

    házi

--- Uivárosi Gábriel/szélsőérék.py
+++ Uivárosi Gábriel/szélsőérék.py
@@ -2,14 +2,23 @@ Y = "Y"
 Liszt = []
 szav = ''
 Leghoszabb = 0
+Legrövidebb = 9999
+szak = 'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz'
 while Y != "":
   Y =  input("Agy meg egy szót")
   Liszt.append(Y)
 for szo in Liszt:
 
-     if len(szo) > Leghoszabb:
+      if len(szo) > Leghoszabb:
        szav = szo
        Leghoszabb = len(szo)
-       
+      else:
+        if len(szo) != 0:
+          if len(szo) < Legrövidebb:
+            szak = szo
+            Legrövidebb = len(szo) 
+print("A lista elemei:",Liszt)       
 print("A leghoszabb szó:",szav)       
 print("Hosza:", Leghoszabb)
+print("Legrövidebb szó:",szak)
+print("Hosza:",Legrövidebb)
