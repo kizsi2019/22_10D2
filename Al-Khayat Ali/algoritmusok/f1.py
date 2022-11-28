378df3f288fdccc9d247d42fb59c03efb18ffd54
Al-Khayat Ali <alkhayyatalii@gmail.com>
2022-11-28 13:33:28 +0100

    eljaras.py

---
+++ Al-Khayat Ali/algoritmusok/f1.py
@@ -0,0 +1,8 @@
+import random
+lista = []
+darab = 0
+for i in range(5):
+    szam = random.randint(1, 10)
+    lista.append(szam)
+for szam2 in lista:
+    if szam2 % 2 == 0
---
+++ Al-Khayat Ali/algoritmusok/faktorialis.py
@@ -0,0 +1,6 @@
+fakt = 1
+szam = int(input('melyik szam faktorialisat szamoljam?'))
+
+for i in range(szam):
+    fakt = fakt*(i+1)
+print(fakt)
