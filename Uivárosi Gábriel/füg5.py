8e5814ac628905b16c4dc75d8f693e7cf13f23be
UivarosiGabriel <gabi455@yahoo.com>
2022-12-05 13:38:27 +0100

    7

--- Uivárosi Gábriel/füg5.py
+++ Uivárosi Gábriel/füg5.py
@@ -5,4 +5,12 @@ def kerulet():
         be = int(input("szám"))
         if be != 0:
             list.append(be)
+    ker = 1
+    szög = 0
+    for szam in list:
+        ker = ker*szam
+        szög = szög+1
+    print("Az ön", szög, "szög e kerülete:", ker)
+kerulet()
+
 
