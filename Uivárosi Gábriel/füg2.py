8974ec5ff0fdf47f0b75bb6f6707bbdc6159337e
UivarosiGabriel <gabi455@yahoo.com>
2022-12-05 13:26:24 +0100

    6

--- Uivárosi Gábriel/füg2.py
+++ Uivárosi Gábriel/füg2.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 list = [2, 3, 4, 5, 6, 23, 21, 22, 9]
 def harommal_oszthatok(x, *args):
     har = 0
@@ -9,15 +9,4 @@ def harommal_oszthatok(x, *args):
 harommal_oszthatok(list)
 
 
-=======
-list = [2, 3, 4, 5, 6, 23, 21, 22, 9]
-def harommal_oszthatok(x, *args):
-    har = 0
-    for szam in list:
-        if szam % 3 == 0:
-            har = har + 1
-    print(har)
-harommal_oszthatok(list)
 
-
->>>>>>> origin/main
