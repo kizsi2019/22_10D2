bb63a60db6098077e3d23fe3ccce8b2066b49890
Gabi4555 <90388665+Gabi4555@users.noreply.github.com>
2022-12-05 19:31:09 +0100

    jav

--- Uivárosi Gábriel/füg5.py
+++ Uivárosi Gábriel/füg5.py
@@ -5,10 +5,10 @@ def kerulet():
         be = int(input("szám"))
         if be != 0:
             list.append(be)
-    ker = 1
+    ker = 0
     szög = 0
     for szam in list:
-        ker = ker*szam
+        ker = ker +szam
         szög = szög+1
     print("Az ön", szög, "szög e kerülete:", ker)
 kerulet()
