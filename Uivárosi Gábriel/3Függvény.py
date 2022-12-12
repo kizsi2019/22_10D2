f38d9948437d4c16c623705e629e3142f67cd935
Gabi4555 <90388665+Gabi4555@users.noreply.github.com>
2022-12-12 13:17:14 +0100

    jj

--- Uivárosi Gábriel/3Függvény.py
+++ Uivárosi Gábriel/3Függvény.py
@@ -1,9 +1,11 @@
 
-list = [2, 3, 4, 5, 6, 23, 21, 22, 9]
+
 def harommal_oszthatok(x, *args):
     har = 0
-    for szam in list:
+    if x % 3 == 0:
+        har = har + 1
+    for szam in args:
         if szam % 3 == 0:
             har = har + 1
     print(har)
-harommal_oszthatok(list)
+harommal_oszthatok(2, 3, 4, 5, 6, 23, 21, 22, 9)
