6af49af7c4cac88ad986c6fbd1ce083a60bd618e
SzakalyAbel <szakalyabel25@gmail.com>
2022-12-12 13:34:24 +0100

    fuggveny, eljarasok

---
+++ Szakály Ábel/eljarasfeladat2.py
@@ -0,0 +1,11 @@
+def szam():
+    if x > 0:
+        print("pozitív")
+    elif x < 0:
+        print("negatív")
+    else:
+        print("nulla")
+
+x = int(input("Adj meg egy számot!"))
+
+szam()
