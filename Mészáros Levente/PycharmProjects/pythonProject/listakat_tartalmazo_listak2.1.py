e8e80a0665c659470630aad3e97433f0398b7fee
meszaros16 <levjuce2004@gmail.com>
2023-01-31 18:02:23 +0100

    zseni vagyok

---
+++ Mészáros Levente/PycharmProjects/pythonProject/listakat_tartalmazo_listak2.1.py
@@ -0,0 +1,19 @@
+def eljaras():
+    tarolo = []
+
+    lista_1 = ['O', 'O', 'O']
+    lista_2 = ['O', 'O', 'O']
+    lista_3 = ['O', 'O', 'O']
+
+    tarolo.append(lista_1)
+    tarolo.append(lista_2)
+    tarolo.append(lista_3)
+
+    for gyasz in tarolo:
+        print(' '.join(gyasz))
+
+
+
+eljaras()
+
+
