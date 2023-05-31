316ee190f02fe2c4776e228b2ad8037cfce98b52
SoundCrafterYT <konor.fuglovics@gmail.com>
2023-05-31 14:18:28 +0200

    Pi

--- Fuglovics Konor/window.py
+++ Fuglovics Konor/window.py
@@ -1,22 +1,27 @@
 import pygame
 
+#Setup
 width, height = 640, 480
 blue = (0, 0, 255)
 black = (0, 0, 0)
 
-
 pygame.init()
 screen = pygame.display.set_mode((width, height))
+font = pygame.font.SysFont("FixSys", 12)
 
+#Load
+img = pygame.image
 
+#Loop
 run = True
 while run:
+    size = 1
     x, y = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if pygame.mouse.get_pressed() == (1, 0, 0):
-            pygame.draw.circle(screen, (128, 128, 128), (x, y), 1)
+            pygame.draw.circle(screen, (128, 128, 128), (x, y), size)
 
     pygame.display.update()
 
