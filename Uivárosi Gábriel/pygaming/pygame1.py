bc1c4920c460d2b61e50a3ef801efeb59df01733
UivarosiGabriel <gabi455@yahoo.com>
2023-05-31 14:13:45 +0200

    f

--- Uivárosi Gábriel/pygaming/pygame1.py
+++ Uivárosi Gábriel/pygaming/pygame1.py
@@ -2,8 +2,10 @@ import pygame
 Game_Running = True
 BLUE = 0, 0 ,255
 WHITE = 255, 255, 255
+WIDTH = 1000
+HEIGHT = 700
 pygame.init()
-screen = pygame.display.set_mode((800, 500))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 rec_pos_x = 100
 res_pos_y = 120
 Go_UP = False
@@ -11,6 +13,10 @@ GO_DOWN = False
 Go_Left = False
 Go_Rigth = False
 clock = pygame.time.Clock()
+conter = 0
+bird_index =  0
+coin_surf = pygame.image.load('img/Coin.png').convert_alpha()
+coin_rec = coin_surf.get_rect(center=(WIDTH - 100, HEIGHT / 2))
 while Game_Running :
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -51,9 +57,19 @@ while Game_Running :
     if Go_Rigth:
         rec_pos_x -= 10
     screen.fill(WHITE)
-    player_Bird = pygame.image.load('img/PNG/frame-1.png')
-    rect = pygame.Rect(rec_pos_x, res_pos_y, 10, 10,)
-    screen.blit(player_Bird, rect)
+    bird1 = pygame.image.load('img/PNG/frame-1.png').convert_alpha()
+    bird2 = pygame.image.load('img/PNG/frame-2.png').convert_alpha()
+    bird3 = pygame.image.load('img/PNG/frame-3.png').convert_alpha()
+    bird4 = pygame.image.load('img/PNG/frame-4.png').convert_alpha()
+    player_Bird = [bird1, bird2, bird3, bird4]
+    conter += 1
+    if conter % 3 == 0:
+        bird_index += 1
+    if bird_index >= 3:
+        bird_index = 0
+    screen.blit(coin_surf, coin_rec)
+    rect = pygame.Rect( rec_pos_x, res_pos_y, 10, 10,)
+    screen.blit(player_Bird[bird_index], rect)
     clock.tick(60)
     pygame.display.update()
 pygame.quit
