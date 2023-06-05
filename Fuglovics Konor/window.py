29ce4e984301c0e61f6e4ed1dd1ff675e4ca4106
SoundCrafterYT <konor.fuglovics@gmail.com>
2023-06-05 11:58:35 +0200

    size

--- Fuglovics Konor/window.py
+++ Fuglovics Konor/window.py
@@ -13,16 +13,19 @@ font = pygame.font.SysFont("FixSys", 12)
 img = pygame.image
 
 #Loop
+size = 1
 run = True
 while run:
-    size = 1
     x, y = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         if pygame.mouse.get_pressed() == (1, 0, 0):
             pygame.draw.circle(screen, (128, 128, 128), (x, y), size)
+        if pygame.mouse.get_pressed() == (0, 1, 0):
+            size = size + 1
+        if pygame.mouse.get_pressed() == (0, 0, 1):
+            pygame.draw.circle(screen, (0, 0, 0), (x, y), size)
 
     pygame.display.update()
-
 pygame.quit()
