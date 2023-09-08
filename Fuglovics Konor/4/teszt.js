6e4b2e61f72f056d82412a87640feee8374372bb
SoundCrafterYT <konor.fuglovics@gmail.com>
2023-09-08 12:59:59 +0200

    now it worky :3

---
+++ Fuglovics Konor/4/teszt.js
@@ -0,0 +1,27 @@
+/*console.log("Hello World!");
+
+var keresztnev = "Konor";
+console.log(keresztnev);
+var vezeteknev = "Fuglovics";
+console.log(vezeteknev);
+
+var kor = 18;
+
+var nagykoru = true;
+console.log(nagykoru);
+
+var magassag;
+console.log(magassag);
+//console.log(nemLetezo);
+
+console.log(keresztnev + " " + kor);
+console.log(vezeteknev + " felnőtt? " + nagykoru);*/
+
+var szam = 10;
+var osszeg = szam + 11;
+console.log(osszeg);
+
+var szam1 = 100;
+var szam2 = 200;
+var egyenlo = szam1 <= szam2;
+console.log(egyenlo);
---
+++ Fuglovics Konor/5/teszt.js
@@ -0,0 +1,15 @@
+var nev = "Koni";
+
+function teszt() {
+    var x = 10;
+    console.log(nev);
+}
+
+teszt();
+
+// ES6
+{
+    var y = 21;
+}
+
+console.log(y)
