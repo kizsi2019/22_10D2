f8303acbe324a13f62d274cedcdecd610af478f3
Szabolcs Nagy <szabolcs_nagy@katonaj-mkszig.hu>
2023-09-08 13:01:25 +0200

    legendák ligája

---
+++ Nagy Szabolcs/JavaScript/3/script.js
@@ -0,0 +1,26 @@
+/*var keresztnév = 'pisti';
+var vezeteknev = 'Kiss';
+
+var magassag;
+var kor = 32;
+var nagykoru = true;
+console.log(keresztnév);
+console.log(vezeteknev);
+console.log(nagykoru);
+console.log(magassag);
+console.log(nemletezo);*/
+
+//asdsadsadsa
+var keresztNev = 'Pisti';
+var kor = 32;
+
+console.log(keresztNev + ' ' + kor);
+
+var vezetekNev, felnott;
+vezetekNev = 'Kovács';
+//felnott = true;
+
+console.log(vezetekNev + ' felnőtt? ' + felnott);
+kor = 'harminc';
+
+console.log(keresztNev + ' ' + kor);
