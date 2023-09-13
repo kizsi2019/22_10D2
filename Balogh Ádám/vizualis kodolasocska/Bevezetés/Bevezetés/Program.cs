a36cc33b5201a7264c7961faeb72175545acd441
Adam Balogh <adam.balogh@katonaj-mkszig.hu>
2023-09-13 09:09:08 +0200

    kys

---
+++ Balogh Ádám/vizualis kodolasocska/Bevezetés/Bevezetés/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bevezetés
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Helló világ");
+            Console.Write("Ez");
+            Console.Write("mi?");*/
+            int a = 7;
+            int b = 3;
+            int ab = a + b;
+            //b = b + 3;
+            b += 3;
+            b -= 2;
+            Console.WriteLine(b);
+            Console.WriteLine(ab);
+            string str1 = "alma";
+            string str2 = "fa";
+            string ossz = str1 + str2;
+            Console.WriteLine(ossz);
+            Console.ReadKey();
+        }
+    }
+}
