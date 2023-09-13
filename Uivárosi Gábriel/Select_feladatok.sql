27753e7a4da537c0f668b2c037fec726d749f410
Gabi4555 <90388665+Gabi4555@users.noreply.github.com>
2023-09-13 14:17:15 +0200

    Select_feladatok

---
+++ Uivárosi Gábriel/Select_feladatok.sql
@@ -0,0 +1,13 @@
+SELECT * From Categories;
+
+SELECT CategoryName AND Description From Categories;
+
+SELECT 	ProductName, Unit, Price FROM Products Where Price < 20
+
+SELECT 	ProductName, Unit, Price FROM Products Where Price Between 20 and 30
+
+SELECT 	ProductName, Unit, Price FROM Products Where Price <> 10 
+
+SELECT 	ProductName, Unit, Price FROM Products Where ProductName like "t%"
+
+SELECT 	ProductName, FROM Products Where ProductName like "Louisiana%"
