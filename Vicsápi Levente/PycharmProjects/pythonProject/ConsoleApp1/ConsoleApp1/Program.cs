7f7bbcc9971c3494c64753b9bebb18549dbf35c3
Levi <levjuce2004@gmail.com>
2023-09-14 17:27:17 +0200

    sql kész

---
+++ Vicsápi Levente/PycharmProjects/pythonProject/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Add meg a nevedet!! ");
+            string nev = Console.ReadLine();
+
+            Console.WriteLine(nev);
+
+            Console.ReadKey();
+        }
+    }
+}
