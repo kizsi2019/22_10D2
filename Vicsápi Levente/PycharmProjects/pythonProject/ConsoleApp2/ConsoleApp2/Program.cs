ddc4ec4ebc5f1e3033dbdefd753892c7c65014ab
Levi <levjuce2004@gmail.com>
2023-09-15 10:24:17 +0200

    kész java

---
+++ Vicsápi Levente/PycharmProjects/pythonProject/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adj meg egy számot! ");
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adj meg egy másik számot! ");
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+
+            if (szam1 > szam2)
+            {
+                Console.WriteLine(szam1 + " nagyobb mint " + szam2);
+            }
+
+            else if (szam1 < szam2)
+            {
+                Console.WriteLine(szam2 + " nagyobb mint " + szam1);
+            }
+
+            else
+            {
+                Console.WriteLine("A két szám egyenlő");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
